1b006e8e413aa834899c3350d873a40935b64898
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-07-20 23:23:22 +0900

    ”Modify” 編集画面の修正　ルートパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :diaries
+  root 'diaries#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
